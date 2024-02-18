689aa1939fb07a2005e708090ba81074556538a6
Parakh Jain <parakhjain2301@gmail.com>
2024-02-18 08:51:43 +0000

    added parallel routing

---
+++ src/app/orgs/[orgId]/layout.tsx
@@ -0,0 +1,17 @@
+export default function Layout({
+  // children,
+  parakh,
+  nirakh,
+}: {
+  // children: React.ReactNode
+  parakh: React.ReactNode
+  nirakh: React.ReactNode
+}) {
+  return (
+    <>
+      {/* {children} */}
+      {parakh}
+      {nirakh}
+    </>
+  )
+}
---
+++ src/app/orgs/page.tsx
@@ -0,0 +1,5 @@
+export default function Orgs(){
+    return (
+        <h1>Orgs</h1>
+    )
+}
